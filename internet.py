9e70ec804b071fd27eef1e809054bc29b57d5576
denishjackson1 <denishkomakech@gmail.com>
2021-09-27 21:32:52 +0300

    Mbps

--- internet.py
+++ internet.py
@@ -4,6 +4,6 @@ st = Speedtest()
 
 st.get_best_server()
 print(f"Your ping is: {st.results.ping} ms")
-print(f"Your download speed: {round(st.download() / 1000 / 1000, 1)} Mbit/s")
-print(f"Your upload speed: {round(st.upload() / 1000 / 1000, 1)} Mbit/s")
+print(f"Your download speed: {round(st.download() / 1000 / 1000, 1)} Mbps")
+print(f"Your upload speed: {round(st.upload() / 1000 / 1000, 1)} Mbps")
 #....
